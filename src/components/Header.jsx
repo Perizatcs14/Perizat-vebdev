0b97ed12a69268e877dc4805f99e4a5adde1aac7
Perizat Baiyzbekova <baiyzbekovaperi@icloud.com>
2024-12-16 15:21:10 +0300

    Добавлена страница бронирования

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/book">Book</Link>
+        <Link to="/book">Book a Tour</Link>
       </nav>
     </header>
   );
